f5afd8ba4d5467728fb1472dba4aaba4f88cd921
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 17:03:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlistObj, artist, title){
 }
 
 function removeFromPlaylist(playlistObj, artist){
-  delete playistObj[artist]
+  delete playlistObj[artist]
 }
 //   describe('updatePlaylist(playlist, artistName, songTitle)', () => {
 //     before(() => {
